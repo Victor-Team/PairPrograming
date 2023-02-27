4908af4187c4f9c96a17e9b970def233820288e6
DarioCode1 <118449641+DarioCode1@users.noreply.github.com>
2023-02-27 19:08:58 +0000

    Add files via upload

---
+++ Trabajo en grupo/Trabajo en grupo/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Drawing;
+using System.IO;
+namespace Ejer4
+{
+    class Program
+    {
+        public const string NOMFICH = "Datos notas.csv";
+        public const string EXITFICH = "Salida.txt";
+        static void Main(string[] args)
+        {
+            StreamReader reader = null;
+            StreamWriter writer = null;
+            try
+            {
+                reader = new StreamReader(NOMFICH);
+                writer = new StreamWriter(EXITFICH);
+                int i = 1;
+                while (!reader.EndOfStream)
+                {
+                    if (i % 2 != 0)
+                    {
+                        string linea = reader.ReadLine();
+                        string[] datos = linea.Split(';');
+                        int[] notas = new int[datos.Length - 1];
+                        for (int j = 0; j < notas.Length; j++) notas[j] = int.Parse(datos[j + 1]);
+                        double notaEjercicios = ((notas[0] + notas[1] + notas[2])/3 * 0.3);
+                        double notaIntervenciones = ((notas[3] + notas[4] + notas[5])/3 * 0.2);
+                        double notaExamenes = (notas[6] + notas[7])/2 * 0.5;
+                        int notaFinal = (int)(notaEjercicios + notaIntervenciones + notaExamenes);
+                        writer.WriteLine(datos[0] + ";"+ notaFinal);
+                        i++;
+                    }
+                    else
+                    {
+                        string linea = reader.ReadLine();
+                        i++;
+                    }
+                }
+                reader.Close();
+                writer.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ Trabajo en grupo/Trabajo en grupo2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+namespace Ejer2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese el nombre del archivo: ");
+            string nombreFichero = Console.ReadLine();
+            while (nombreFichero.Length < 1)
+            {
+                Console.WriteLine("El nombre no puede estar vacio");
+                nombreFichero = Console.ReadLine();
+            }
+            Console.Write("Ingrese el número de líneas a mostrar: ");
+            int n;
+            while ((!int.TryParse(Console.ReadLine(), out n)) || (n < 0))
+                    Console.WriteLine("Tiene que ser un numero positivo");
+            try
+            {
+                List<string> lineas = File.ReadAllLines(nombreFichero).ToList();
+
+                if (n < lineas.Count)
+                    for (int i = 0; i < n; i++)
+                        Console.WriteLine(lineas[lineas.Count - n + i]);
+                else
+                    foreach (string line in lineas)
+                        Console.WriteLine(line);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
